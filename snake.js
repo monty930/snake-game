78f7206375c68b3fc66b380cb4fa2e7eb3c871ee
Krystyna Gasińska <71830127+monty930@users.noreply.github.com>
2021-02-08 21:15:48 +0100

    added blue apples, fixed bugs
    better arrow control handle, added new type of apple, fixed score counting bugs

--- snake.js
+++ snake.js
@@ -9,6 +9,7 @@ const SQ = squareSize = 20; // size of single square
 const VACANT = "LAWNGREEN"; // canvas default colour
 const SNAKESQUARE = "GREEN"; // snake colour
 const APPLE = "RED"; // apple colour
+const BLUEAPPLE = "BLUE"; // blue apple colour
 
 
 // draw a square
@@ -22,7 +23,7 @@ function drawSquare (x, y, colour) { // coordinates and colour of square
   ctx.strokeRect(x*SQ, y*SQ, SQ, SQ); // draw stroke: coordinates and size of square
 }
 
-let snakeArray = []; // array of snake - squares (1) vacants (0) and food (2)
+let snakeArray = []; // array of snake - squares (1) vacants (0) apple (2) blueapple (3)
 let liveArray = []; //array of live duration of each snake - squares
 
 function zeroArray () {
@@ -43,11 +44,16 @@ for (i = 0; i <ROW; i++) {
   }
 }
 
+// move or extend the snake according to live board
 function actualizeBoard (mode) {
   for (i = 0; i < ROW; i++) {
     for (j = 0; j < COL; j++) {
-        if (liveArray[i][j] == 1 && mode == -1) snakeArray[i][j] = 0;
+        if (liveArray[i][j] + mode == 0 && mode != 0) snakeArray[i][j] = 0;
         if (liveArray[i][j] > 0) liveArray[i][j] = liveArray[i][j] + mode;
+        if (liveArray[i][j] < 0 ) {
+          snakeArray[i][j] = 0;
+          liveArray[i][j] = 0;
+        }
     }
   }
 }
@@ -59,52 +65,49 @@ function drawBoard () {
           if (snakeArray[i][j] == 0) drawSquare(i, j, VACANT);
           if (snakeArray[i][j] == 1) drawSquare(i, j, SNAKESQUARE);
           if (snakeArray[i][j] == 2) drawSquare(i, j, APPLE);
+          if (snakeArray[i][j] == 3) drawSquare(i, j, BLUEAPPLE);
       }
   }
 }
 
 drawBoard();
 
-let snakeLength = 8;
+let snakeLength = 1;
+
+let blueAppleProb = 0;
 
 // generate random square number not being snake - square
 function randomSquare () { 
-  let r = randomN = Math.floor(Math.random() * (ROW * COL - snakeLength));
+  let r = Math.floor(Math.random() * (ROW * COL - snakeLength - 1));
+  let typeRandom = Math.floor(Math.random() * 1000) + 1;
+  let type = "APPLE";
+  if (typeRandom < blueAppleProb) {
+    type = "BLUEAPPLE";
+    blueAppleProb = 0;
+  }
+  else blueAppleProb = blueAppleProb + 100;
   let counter = 0;
   for (i = 0; i < ROW; i++) {
-      for (j = 0; j < COL; j++) {
-          if (snakeArray[i][j] == 0) {
-              if (counter === r) return {corx: i, cory: j};
-              counter++;
-          }
+    for (j = 0; j < COL; j++) {
+      if (snakeArray[i][j] == 0) {
+        if (counter === r) return {corx: i, cory: j, appleType: type};
+        counter++;
       }
+    }
   }
   console.log("error");
   errors.innerHTML = "generate random numbers error!!!";
-  return {corx: 0, cory: 0};
+  return {corx: 0, cory: 0, appleType: type};
 }
 
+// snake's head direction
 let dir = "EAST";
 
 // snake's head coordinates
-let snakeHead = {corx: 10, cory: 10};
-snakeArray[3][10] = 1;
-snakeArray[4][10] = 1;
-snakeArray[5][10] = 1;
-snakeArray[6][10] = 1;
-snakeArray[7][10] = 1;
-snakeArray[8][10] = 1;
-snakeArray[9][10] = 1;
-snakeArray[10][10] = 1;
-
-liveArray[3][10] = 1;
-liveArray[4][10] = 2;
-liveArray[5][10] = 3;
-liveArray[6][10] = 4;
-liveArray[7][10] = 5;
-liveArray[8][10] = 6;
-liveArray[9][10] = 7;
-liveArray[10][10] = 8;
+let center = {corx: Math.floor(0.5 * COL), cory: Math.floor(0.5 * ROW)};
+let snakeHead = {corx: center.corx, cory: center.cory};
+snakeArray[snakeHead.corx][snakeHead.cory] = 1;
+liveArray[snakeHead.corx][snakeHead.cory] = 1;
 
 // check if game is over
 let gameOver = false;
@@ -118,21 +121,37 @@ function checkCollision () {
 
 drawBoard();
 
+// score counter
 let score = 0;
 
+// checks if snake ate an apple. if so, creates new one.
 function checkApple (isInit) {
-  if (snakeArray[snakeHead.corx][snakeHead.cory] == 2 || isInit) {
+  if (snakeArray[snakeHead.corx][snakeHead.cory] > 1 || isInit) {
     let apple = randomSquare();
-    snakeArray[apple.corx][apple.cory] = 2;
-    snakeLength = snakeLength + 3;
-    actualizeBoard(3);
-    if (!isInit) {
+    if (apple.appleType == "APPLE") snakeArray[apple.corx][apple.cory] = 2;
+    else if (apple.appleType == "BLUEAPPLE") snakeArray[apple.corx][apple.cory] = 3;
+    else {
+      console.log("error");
+      errors.innerHTML = "apple type error!!!";
+    }
+    if (!isInit && snakeArray[snakeHead.corx][snakeHead.cory] == 2) {
+      snakeLength = snakeLength + 3;
+      actualizeBoard(3);
       score += snakeLength - 3;
+      countScore.innerHTML = score;
+    }
+    else if (!isInit && snakeArray[snakeHead.corx][snakeHead.cory] == 3) {
+      let tmpScore = Math.floor(0.7 * snakeLength);
+      let oldLength = snakeLength;
+      snakeLength = Math.max(Math.floor(0.3 * snakeLength), 1);
+      actualizeBoard(snakeLength - oldLength);
+      score += tmpScore;
       countScore.innerHTML = score;
     }
   }
 }
 
+// init apple
 checkApple (true);
 
 // moves the snake in indicated direction
@@ -156,13 +175,18 @@ function moveSnake () {
   if (!gameOver) {
     checkApple(false);
     snakeArray[snakeHead.corx][snakeHead.cory] = 1;
-    liveArray[snakeHead.corx][snakeHead.cory] = snakeLength;
+    liveArray[snakeHead.corx][snakeHead.cory] = snakeLength+1;
     actualizeBoard (-1);
     drawBoard();
   }
 }
 
+// counter of game-over graphics
 let gameOverAnimation = 0;
+
+// handle arrow press frequency
+let arrowPress = true;
+let arrowQueue = -1;
 
 // main game loop
 let loopStart = Date.now ();
@@ -170,6 +194,10 @@ function mainGameLoop () {
   let now = Date.now ();
   let dt = now - loopStart;
   if(dt > 100){
+    if (arrowQueue != -1 && arrowPress == true) {
+      CONTROL ({keyCode: (-1) * arrowQueue})
+    }
+    arrowPress = true;
     if (!gameOver) moveSnake ();
     else if (gameOverAnimation < 7) {
       handleGameOver (gameOverAnimation % 2);
@@ -180,8 +208,7 @@ function mainGameLoop () {
   requestAnimationFrame (mainGameLoop);
 };
 
-mainGameLoop();
-
+// shows game-over graphics
 function handleGameOver (mode) {
   if (mode === 1) zeroArray ();
   else {
@@ -194,21 +221,34 @@ function handleGameOver (mode) {
   drawBoard();
 }
 
+// arrow control handle
 document.addEventListener("keydown", CONTROL);
 
 function CONTROL (event) {
   if (!gameOver) {
-    if(event.keyCode == 37) {
+    if(arrowPress == false && arrowQueue == -1) {
+      arrowQueue = event.keyCode;
+    }
+    else if (arrowPress == true) {
+      arrowPress = false;
+      if (event.keyCode < 0) arrowQueue = -1;
+      if(event.keyCode == 37 || event.keyCode == -37) {
         if (dir != "EAST" || snakeLength == 1) dir = "WEST";
-    }
-    if(event.keyCode == 38) {
+      }
+      if(event.keyCode == 38 || event.keyCode == -38) {
         if (dir != "SOUTH" || snakeLength == 1) dir = "NORTH";
-    }
-    if(event.keyCode == 39) {
+      }
+      if(event.keyCode == 39 || event.keyCode == -39) {
         if (dir != "WEST" || snakeLength == 1) dir = "EAST";
-    }
-    if(event.keyCode == 40) {
+      }
+      if(event.keyCode == 40 || event.keyCode == -40) {
         if (dir != "NORTH" || snakeLength == 1) dir = "SOUTH";
+      }
+    }
+    else {
+      console.log("uwaga, nacisnieto wiecej strzalek:");
+      console.log(arrowQueue);
+      console.log(event.keyCode);
     }
   }
 };
